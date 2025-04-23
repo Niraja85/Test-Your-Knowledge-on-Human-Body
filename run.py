58c42e03238f6860673842014fc0cfda6ffd21e3
Niraja85 <tripathi.niraja@gmail.com>
2025-04-23 23:54:05 +0200

    Added The welcome message for the quiz

--- run.py
+++ run.py
@@ -3,3 +3,21 @@
 import colorama
 from colorama import Fore, Back, Style
 import pyfiglet
+
+""" Initialise colorama"""
+colorama.init()
+
+"""
+A welcome statement for starting the quiz and if the user
+says no, the game quits, and if chooses yes to play the game, 
+questions are displayed.
+"""
+welcome = pyfiglet.figlet_format("Welcome To The Quiz World !")
+print(welcome)
+
+ans = input("Are you ready to play?\n").lower()
+
+if ans != "yes":
+    quit()
+
+score = 0
