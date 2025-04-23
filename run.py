035f73ff08da899596d6397f55a1554942194c25
Niraja85 <tripathi.niraja@gmail.com>
2025-04-24 00:31:38 +0200

    Created a dictionary using questions and its options as key and correct answer as value pair

--- run.py
+++ run.py
@@ -15,9 +15,95 @@ questions are displayed.
 welcome = pyfiglet.figlet_format("Welcome To The Quiz World !")
 print(welcome)
 
-ans = input("Are you ready to play?\n").lower()
+ans = input("Are you ready to play? (yes/no)\n").lower()
 
 if ans != "yes":
     quit()
 
 score = 0
+
+"""
+Creating variables from q1 to q10 which includes questions and choices,
+that will act as key in dictionary named questions. 
+"""
+q1 = """ Because its cells has mitochondria,which of your muscles never tires?
+a. Tongue
+b. Triceps
+c. Heart
+d. Gluteus Maximus
+"""
+q2 = """ The paper-thin tympanic membrane can be found in what body part?
+a. Ear
+b. Colon
+c. Lungs
+d. Throat
+"""
+q3 = """ Which of these body parts has the most bones?
+a. Rib-cage
+b. Foot
+c. Skull
+d. Hand
+"""
+q4 = """ Where are red blood cells created?
+a. Brain
+b. Heart
+c. Spleen
+d. Bones
+"""
+q5 = """ Which of these body parts continue to get bigger with age?
+a. Ear
+b. Thumb
+c. Spine
+d. Femur
+"""
+q6 = """ The funny bone is actually what type of body-part?
+a. Nerve
+b. Muscle
+c. Bone
+d. Organ
+"""
+q7 = """ How many bones are there in human body?
+a. 260
+b. 206
+c. 150
+d. 200
+"""
+q8 = """ What does mucus do?
+a. Filters out harmful bacteria.
+b. Helps in smell
+c. Helps your lungs take in oxygen
+d. Stops your nose from collapsing
+"""
+q9 = """ The most common eye color in the world is:
+a. Brown
+b. Blue
+c. Green
+d. Hazel
+"""
+q10 = """ How many muscles do you use when you talk?
+a. 45
+b. 55
+c. 72
+d. 87
+"""
+
+"""
+Created a dictionary with q1 to q10 as 'key' and correct answer as 'value'
+"""
+questions = {
+    q1: "c",
+    q2: "a",
+    q3: "d",
+    q4: "d",
+    q5: "a",
+    q6: "a",
+    q7: "b",
+    q8: "a",
+    q9: "a",
+    q10: "c"
+}
+"""
+Creating a for loop to iterate the question and its correct answers.
+"""
+for question in questions:
+    print(question)
